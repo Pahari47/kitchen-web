f89d128ecf20092c10e3c30fd6924d2ec6fad30c
Pahari47 <nachiketapahari00@gmail.com>
2024-03-02 03:43:42 +0530

    feature added

--- main page/script.js
+++ main page/script.js
@@ -1,4 +1,34 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
+
+function page3Animation(){
+    var elemC = document.querySelector("#elem-container");
+var fix = document.querySelector("#fixed-image");
+elemC.addEventListener("mouseenter", function () {
+  fix.style.display = "block";
+});
+elemC.addEventListener("mouseleave", function () {
+  fix.style.display = "none";
+});
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    var image = e.getAttribute("data-image");
+    fix.style.backgroundImage = `url(${image})`;
+  });
+});
+}
+
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+      });
+}
+
+swiperAnimation()
+page3Animation()
